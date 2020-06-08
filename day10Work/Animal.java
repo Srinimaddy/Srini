74565d37bba519935cef3de6e39bdd55b3df8c30
Srinimaddy <66350330+Srinimaddy@users.noreply.github.com>
2020-06-08 12:06:23 +0530

    Add files via upload

---
+++ day10Work/Animal.java
@@ -0,0 +1,15 @@
+package day10Work;
+
+public class Animal {
+	
+	void eat() {
+		System.out.println("All animals will eat");
+	}
+	void move() {
+		System.out.println("All animals will move");
+	}
+	void sleep() {
+		System.out.println("All animals will sleep");
+	}
+
+}
---
+++ day10Work/Monkey.java
@@ -0,0 +1,10 @@
+package day10Work;
+
+public class Monkey extends Animal {
+	
+	@Override
+	void move() {
+		System.out.println("Monkey will eat bananas!!!!!");
+	}
+
+}
