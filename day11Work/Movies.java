5a43cd095c2fb755c3352c8f4eff563a13481bb6
Srinimaddy <66350330+Srinimaddy@users.noreply.github.com>
2020-06-09 12:06:53 +0530

    Add files via upload

---
+++ day11Work/Movies.java
@@ -0,0 +1,82 @@
+package day11Work;
+
+import java.util.Scanner;
+
+public class Movies {
+	private int id;
+	private String name;
+	private float duration;
+
+	Scanner scanner;
+
+	//default
+	Movies(){
+		scanner = new Scanner(System.in);
+	}
+	//overloaded constructors
+	Movies(String name,float duration){
+		scanner = new Scanner(System.in);
+		this.name = name;
+		this.duration = duration;
+	}
+
+	Movies(int id,String name, float duration)
+	{
+		scanner = new Scanner(System.in);
+		this.id = id;
+		this.name = name;
+		this.duration = duration;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getDuration() {
+		return duration;
+	}
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+	void getMovieDetailsFromUser() {
+		System.out.println("Please enter the movie id");
+		id = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Please enter the movie name");
+		name = scanner.nextLine();
+		System.out.println("Please enter the movie duration");
+		duration = scanner.nextFloat();
+	}
+	/*void printMovieDetails() {
+		System.out.println("Movie ID "+id+"\nMovie Name "+name+"\nMovie Duration "+duration);
+		System.out.println("------------------------------------");
+	}*/
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Movie ID "+id+"\nMovie Name "+name+"\nMovie Duration "+duration;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Movies m2 = (Movies)obj;
+		Movies m1 = this;
+		if(m1.getName().equals(m2.getName()) && m1.getDuration()==m2.getDuration())
+			return true;
+		else
+			return false;
+	}
+}
+	
+			
+		
+
